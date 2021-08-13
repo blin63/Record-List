09e4dfb529f4149a43bc5f06350a464cb93cddc4
Brendan Lin <blin63@my.bcit.ca>
2021-08-12 17:54:36 -0700

    added list.js and listDetails.js with modifications needed to allow list and listDetails HTML pages to read from db

---
+++ js/list.js
@@ -0,0 +1,38 @@
+/**
+ * Gets the user's created lists and displays the lists to view in more detail.
+ * Author: Brendan Lin
+ * Ver: 1.0
+ */
+
+$(document).ready(function () {
+    // Create the table variable w/ header
+    var table = "<table class='table table-hover'><thead><tr><th scope='col'>List Name</th></tr</thead><tbody>";
+
+    //get user's uid
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            console.log(uid);
+
+            //Query user's list from db
+            var listData = db.collection("lists").where("uid", "==", uid).get()
+                .then(function (query) {
+                    query.forEach(function (doc) {
+                        table = table + "<tr><td><a href='listDetails.html?title="+ doc.data().title +"'>" + doc.data().title + "</a></td></tr>";
+                    })
+                }).then(function () {
+                    //close the table
+                    table = table + "</tbody></table>";
+
+                    //append the table to the HTML card body
+                    $("#tableContent").append(table);
+                });
+
+        } else {
+            // User is signed out
+            // ...
+        }
+    });
+});
---
+++ js/listDetails.js
@@ -0,0 +1,71 @@
+/**
+ * Displays the list selected
+ * Author: Brendan Lin
+ * Ver: 1.0
+ */
+
+$(document).ready(function () {
+    // Create the table variable w/ header
+    var table = "<table class='table'><thead><tr><th scope='col'>Type</th><th scope='col'>List Item</th></tr</thead><tbody>";
+
+    //Get the title stored in the URL
+    const param = new URLSearchParams(window.location.search);
+    var title = param.get("title");
+
+    //Variable for list type
+    var type;
+
+    //listItem array variable
+    var item = 0;
+
+    $("#titleName").append(title);
+
+    //get user's uid
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            console.log(uid);
+
+            //Get the List from the db
+            db.collection("lists").where("uid", "==", uid).get()
+                .then(function (query) {
+                    query.forEach(function (doc) {
+                        if (doc.data().title == title) {
+
+                            //Select List Type
+                            switch (doc.data().type) {
+                                //1: Checklist Type
+                                //2: Regular Type
+                                case "1":
+                                    type = "<input class='form-check-input' type='checkbox' value='' id='flexCheckDefault'></input>";
+                                    break;
+
+                                default:
+                                    type = 1;
+                                    break;
+                            }
+
+                            doc.data().listItem.forEach(function () {
+                                table = table + "<tr><td>" + type + "</td><td>" + doc.data().listItem[item] + "</td></tr>";
+                                item++;
+
+                                //Check if the type is a regular list
+                                if (Number.isInteger(type)) {
+                                    type++;
+                                }
+                            });
+
+                        }
+                    })
+                }).then(function () {
+                    //close the table and append to HTML page
+                    table = table + "</tbody></table>";
+
+                    $("#tableContent").append(table);
+                });
+
+        }
+    });
+});
